f265c62403d659143b99416f2fe7110344c02175
kassiopatrik@outlook.com <kassiopatrik@outlook.com>
2021-05-08 23:27:36 -0300

    Instalando boostrap forms e atualizando requirements.txt

--- projetoblog-main/blog/blog/settings.py
+++ projetoblog-main/blog/blog/settings.py
@@ -30,15 +30,26 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DEFAULT_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+MY_APPS = [
     'blog_v1',
 ]
+
+THIRDS_APPS = [
+    'bootstrapform',
+    'adminlte3',
+    'adminlte3_theme',
+]
+
+INSTALLED_APPS = DEFAULT_APPS + MY_APPS + THIRDS_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
